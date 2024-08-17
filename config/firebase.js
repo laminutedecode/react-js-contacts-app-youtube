857da04689d55d6515c4e8cdccae36842b1ad90b
Jbtzcodes <laminutedecode@gmail.com>
2024-08-17 03:05:26 +0200

    UPDATE firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -4,12 +4,7 @@ import { getFirestore } from 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6LswHiSpt5fUt3EUpG7GHKqx5qAP_m3M",
-  authDomain: "test-fee33.firebaseapp.com",
-  projectId: "test-fee33",
-  storageBucket: "test-fee33.appspot.com",
-  messagingSenderId: "566133096052",
-  appId: "1:566133096052:web:3458f9058c9a85b0315b21"
+
 };
 
 
